d780c3831853884866185899f77b17da96d510fc
Anton Mishkevich <mian19@protonmail.com>
2022-12-24 18:21:37 +0300

    init: start project

---
+++ ExpenseTracker/ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Kyzu on 24.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
